373452fd4fd7206b02d04e24ec9a89f7a998f039
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-03-23 18:10:07 -0600

    Soup, salad and fish content added

--- DinerMenuForm.vb
+++ DinerMenuForm.vb
@@ -8,5 +8,26 @@
     '[ ]Set fish button function
     '[ ]Set Exit button function
     '[ ]Display each plate description
+    Sub SoupContent()
+        DisplaySpecialLabel.Text = "Indulge in Venezuelan comfort with our Andean Chicken Chupe. 
+A savory blend of tender chicken, potatoes, and sweet corn, 
+simmered to perfection in a flavorful broth. 
+Topped with creamy table cream and fresh cheese squares, it's a heartwarming delight for any occasion."
+    End Sub
+    Sub SaladContent()
+        DisplaySpecialLabel.Text = "Elevate your salad experience with our Venezuelan Chicken Salad. 
+Succulent grilled chicken breast, tender mixed greens, and vibrant vegetables come together in a symphony 
+of flavors. 
+Tossed in a zesty citrus vinaigrette and topped with crispy plantain strips, 
+it's a refreshing journey through the vibrant flavors of Venezuela. 
+Embrace freshness and sophistication in every bite."
+    End Sub
+    Sub FishContent()
+        DisplaySpecialLabel.Text = "Enjoy the authentic flavors of Venezuela with our Fried Fish paired with crispy Tostones. 
+Tender fish fillets, marinated in garlic and seasoning, are fried to golden perfection. 
+Served with traditional Tostones, thinly sliced green bananas fried until crispy and sprinkled with salt. 
+Accompanied by a refreshing cabbage and carrot salad tossed in creamy mayonnaise dressing. 
+Dive into this classic dish for a taste of Venezuelan culinary delight."
+    End Sub
 
 End Class
