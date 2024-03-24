73f20d6cf5d457c0d769d42c30d6450afbbdbcb4
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-03-23 18:09:38 -0600

    Add project files.

---
+++ DinerMenuForm.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(12, 360)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(122, 64)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(140, 360)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(122, 64)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(268, 360)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(122, 64)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(839, 360)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(122, 64)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Times New Roman", 25.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(322, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(293, 49)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Pichon's Diner"
+        Me.DinerNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.AutoSize = True
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(34, 139)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(0, 28)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(973, 450)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
+End Class
---
+++ DinerMenuForm.vb
@@ -0,0 +1,12 @@
+﻿Public Class DinerMenuForm
+    'TODO
+    '[x]Set Soup Content
+    '[x]Set Salad Content
+    '[x]Set Fish Content
+    '[ ]Set Soup button function
+    '[ ]Set salad button function
+    '[ ]Set fish button function
+    '[ ]Set Exit button function
+    '[ ]Display each plate description
+
+End Class
